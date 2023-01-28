ba5f67e88f6635d5b3d496563daff80c0253f6a8
Miguelco23 <miguelco23vzv@gmail.com>
2023-01-28 15:45:46 -0500

    API working and machine done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
 import './App.css';
-import { Container } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+
+  const [quote, setQuote] = useState('');
+
+  const getQuote = () => {
+    fetch("https://type.fit/api/quotes")
+    .then(response => response.json())
+    .then(data => {
+      let randomNumber = Math.floor(Math.random() * data.length);
+      setQuote(data[randomNumber]);
+    });
+  };
+
+  useEffect(() => {
+    getQuote();
+  },[]);
   return (
     <div className="App">
-      <Container id='quote-box'>
-        <div id="text">
-        <i class="fa fa-quote-left" />
-        <span>Olaa</span>
-        <i class="fa fa-quote-right" />
-        </div>
-        <div id="author"></div>
-        <button id="new-quote">Random Quote</button>
-        <a href="twitter.com/intent/tweet" title='Tweet the quote!' id="tweet-quote"><i class="fa-brands fa-square-twitter" /></a>
-      </Container>
+      <div id='quote-box'>
+      <Card>
+          <Card.Body>
+            <Card.Title id="quote">
+              <i className='fa fa-quote-left'/>
+              {quote.text}
+              <i className='fa fa-quote-right'/>
+            </Card.Title>
+            <Card.Text id="author" className="card-text">{quote.author}</Card.Text> 
+            <Button onClick={getQuote} variant="primary" id="new-quote" style={{"margin-right": "40px"}}>Random Quote</Button>
+            <a href={`https://www.twitter.com/intent/tweet?text=${quote.text}`} title='Tweet the quote!' target="_blank" id="tweet-quote"><i class="fa-brands fa-square-twitter" /></a>
+          </Card.Body>
+        </Card>
+      </div>
     </div>
   );
 }
